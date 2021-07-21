2e21f7b9f46dd501ad7604d61d7aa5f2cae295c0
MartMbithi <martdevelopers254@gmail.com>
2021-07-21 12:56:11 +0300

    Analytics On Student Module

--- partials/student_analytics.php
+++ partials/student_analytics.php
@@ -19,3 +19,40 @@
  * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
  * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
+
+$Login = $_SESSION['Login_id'];
+$ret = "SELECT * FROM student WHERE Student_Login_id = '$Login' ";
+$stmt = $mysqli->prepare($ret);
+$stmt->execute(); //ok
+$res = $stmt->get_result();
+while ($std = $res->fetch_object()) {
+
+    /* Job Applications */
+    $query = "SELECT COUNT(*)  FROM `applications` WHERE Application_Student_id = '$std->Student_Id' ";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($applications);
+    $stmt->fetch();
+    $stmt->close();
+
+    /* Shortlisted Applications */
+    $query =
+        "SELECT COUNT(*)  FROM 
+        student s INNER JOIN applications A
+        ON A.Application_Student_id = s.Student_Id 
+        INNER JOIN shortlisting S
+        ON S.Shortlisting_Application_id = A.Application_id
+        WHERE s.Student_id ='$std->Student_Id'  ";
+    $stmt = $mysqli->prepare($query);
+    $stmt->execute();
+    $stmt->bind_result($shortlisted);
+    $stmt->fetch();
+    $stmt->close();
+}
+/* Posted Jobs */
+$query = "SELECT COUNT(*)  FROM `job` ";
+$stmt = $mysqli->prepare($query);
+$stmt->execute();
+$stmt->bind_result($posted_jobs);
+$stmt->fetch();
+$stmt->close();
