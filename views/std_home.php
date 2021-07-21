ed9174f51369cd203c094fadea4622f188101a94
MartMbithi <martdevelopers254@gmail.com>
2021-07-21 12:48:55 +0300

    Create Student Home

---
+++ views/std_home.php
@@ -0,0 +1,178 @@
+<?php
+/*
+ * Created on Wed Jul 21 2021
+ *
+ * The MIT License (MIT)
+ * Copyright (c) 2021 MartDevelopers Inc
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial
+ * portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
+ * TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+session_start();
+require_once('../config/config.php');
+require_once('../config/checklogin.php');
+checklogin();
+require_once('../partials/std_analytics.php');
+require_once('../partials/head.php');
+?>
+
+<body class="hold-transition sidebar-mini layout-fixed layout-navbar-fixed layout-footer-fixed">
+    <div class="wrapper">
+        <!-- Navbar -->
+        <?php require_once('../partials/navbar.php'); ?>
+        <!-- /.navbar -->
+
+        <!-- Main Sidebar Container -->
+        <?php require_once('../partials/sidebar.php'); ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0 text-dark">Student Dashboard</h1>
+                        </div><!-- /.col -->
+                        <div class="col-sm-6">
+                            <ol class="breadcrumb float-sm-right">
+                                <li class="breadcrumb-item"><a href="std_home">Home</a></li>
+                                <li class="breadcrumb-item active">Dashboard</li>
+                            </ol>
+                        </div><!-- /.col -->
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Info boxes -->
+                    <div class="row">
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="info-box">
+                                <span class="info-box-icon bg-info elevation-1"><i class="fas fa-clipboard-list"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">Posted Jobs</span>
+                                    <span class="info-box-number">
+                                        <?php echo $posted_jobs; ?>
+                                    </span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-danger elevation-1"><i class="fas fa-users"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">My Applications</span>
+                                    <span class="info-box-number"><?php echo $applications; ?></span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+
+                        <div class="clearfix hidden-md-up"></div>
+
+                        <div class="col-12 col-sm-6 col-md-4">
+                            <div class="info-box mb-3">
+                                <span class="info-box-icon bg-success elevation-1"><i class="fas fa-user-check"></i></span>
+
+                                <div class="info-box-content">
+                                    <span class="info-box-text">My Shortlisted Applications</span>
+                                    <span class="info-box-number"><?php echo $shortlisted; ?></span>
+                                </div>
+                                <!-- /.info-box-content -->
+                            </div>
+                            <!-- /.info-box -->
+                        </div>
+
+                        <div class="card col-12">
+                            <div class="card-header border-transparent">
+                                <h3 class="text-center">Latest Job Opening Posts</h3>
+                                <hr>
+                            </div>
+                            <div class="card-body p-0">
+                                <div class="table-responsive">
+                                    <table class="table m-0">
+                                        <thead>
+                                            <tr>
+                                                <th>Job Title</th>
+                                                <th>Company Hiring</th>
+                                                <th>Job Category</th>
+                                                <th>Job Location</th>
+                                                <th>Job Dates</th>
+                                                <th>No Of Vacancies</th>
+                                                <th>Action</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php
+                                            $ret = "SELECT * FROM job j INNER JOIN company c ON c.Company_id = j.Job_Company_id ";
+                                            $stmt = $mysqli->prepare($ret);
+                                            $stmt->execute(); //ok
+                                            $res = $stmt->get_result();
+                                            while ($jobs = $res->fetch_object()) {
+                                            ?>
+                                                <tr>
+                                                    <td><?php echo $jobs->Job_title; ?></td>
+                                                    <td>
+                                                        Name: <?php echo $jobs->Company_name; ?><br>
+                                                        Location: <?php echo $jobs->Company_location; ?><br>
+                                                        Contact : <?php echo $jobs->Company_contact; ?><br>
+                                                        Email : <?php echo $jobs->Company_email; ?>
+                                                    </td>
+                                                    <td><?php echo $jobs->Job_Category; ?></td>
+                                                    <td><?php echo $jobs->Job_location; ?></td>
+                                                    <td>
+                                                        Application Date: <?php echo date('d M Y', strtotime($jobs->Job_apply_date)); ?><br>
+                                                        Closing Date :<?php echo date('d M Y', strtotime($jobs->Job_Last_application_date)); ?>
+                                                    </td>
+                                                    <td><?php echo $jobs->Job_No_of_vacancy; ?></td>
+                                                    <td>
+                                                        <a class="badge badge-success" href="std_job_details?view=<?php echo $jobs->Job_id; ?>">
+                                                            <i class="fas fa-eye"></i>
+                                                            View Details
+                                                        </a>
+                                                    </td>
+                                                </tr>
+                                            <?php
+                                            } ?>
+                                        </tbody>
+                                    </table>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- /.content -->
+        </div>
+        <!-- /.content-wrapper -->
+
+        <!-- Main Footer -->
+        <?php require_once('../partials/footer.php'); ?>
+    </div>
+    <!-- ./wrapper -->
+
+    <?php require_once('../partials/scripts.php'); ?>
+</body>
+
+</html>
